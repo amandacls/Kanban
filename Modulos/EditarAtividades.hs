a36ae2ce1d9d099b6a7ba5b56c607e9a418d802d
ailtonjaj <ailton.junior@ccc.ufcg.edu.br>
2023-05-11 14:49:31 -0300

    Subindo para o main

---
+++ Modulos/EditarAtividades.hs
@@ -0,0 +1,44 @@
+module EditarAtividades where
+
+import Data.List (find)
+import Data.Maybe (fromMaybe)
+import System.IO
+
+import qualified Atividade as Atividade
+import qualified Utils as Utils
+
+
+editarAtividade :: Int -> Maybe String -> Maybe String -> Maybe String -> Maybe String -> IO ()
+editarAtividade idAtividade novoNome mNovaDificuldade mNovaUrgencia mNovaEntrega = do
+    withFile "atividades.txt" ReadWriteMode $ \handle -> do
+        arquivo <- hGetContents handle
+        let atividades = read arquivo :: [Atividade.Atividade]
+
+        let atividadeAtualizada = case find (\a -> idAtividade == Atividade.idAtividade a) atividades of
+                                    Just atividade -> atividade { Atividade.nomeAtividade = fromMaybe (Atividade.nomeAtividade atividade) novoNome
+                                                              , Atividade.dificuldade = fromMaybe (Atividade.dificuldade atividade) mNovaDificuldade
+                                                              , Atividade.urgencia = fromMaybe (Atividade.urgencia atividade) mNovaUrgencia
+                                                              , Atividade.entrega = fromMaybe (Atividade.entrega atividade) mNovaEntrega }
+                                    Nothing -> error "Atividade não encontrada"
+
+        let atividadesAtualizadas = map (\a -> if idAtividade == Atividade.idAtividade a then atividadeAtualizada else a) atividades
+
+        hSeek handle AbsoluteSeek 0
+        hPutStr handle (unlines (map show atividadesAtualizadas))
+        hFlush handle
+
+editarStatus :: Int -> Maybe String -> IO ()
+editarStatus idAtividade mNovoStatus = do
+    withFile "atividades.txt" ReadWriteMode $ \handle -> do
+        arquivo <- hGetContents handle
+        let atividades = read arquivo :: [Atividade.Atividade]
+
+        let atividadeAtualizada = case find (\a -> idAtividade == Atividade.idAtividade a) atividades of
+                                    Just atividade -> atividade { Atividade.status = fromMaybe (Atividade.status atividade) mNovoStatus }
+                                    Nothing -> error "Atividade não encontrada"
+
+        let atividadesAtualizadas = map (\a -> if idAtividade == Atividade.idAtividade a then atividadeAtualizada else a) atividades
+
+        hSeek handle AbsoluteSeek 0
+        hPutStr handle (unlines (map show atividadesAtualizadas))
+        hFlush handle
---
+++ Modulos/ExibirQuadro.hs
@@ -0,0 +1,34 @@
+module ExibirQuadro where
+
+import qualified Atividade as Atividade
+
+import System.IO
+
+exibirQuadro :: IO ()
+exibirQuadro = do
+  putStrLn "Quadro de Atividades"
+  putStrLn ""
+  putStrLn "A fazer: "
+  withFile "atividades.txt" ReadMode (\handle -> do
+    contents <- hGetContents handle
+    let linhas = lines contents
+        atividadesAFazer = filter (\linha -> (Atividade.status (read linha :: Atividade.Atividade)) == "A fazer") linhas
+    mapM_ putStrLn atividadesAFazer)
+  putStrLn "Em andamento: "
+  withFile "atividades.txt" ReadMode (\handle -> do
+    contents <- hGetContents handle
+    let linhas = lines contents
+        atividadesEmAndamento = filter (\linha -> (Atividade.status (read linha :: Atividade.Atividade)) == "Em andamento") linhas
+    mapM_ putStrLn atividadesEmAndamento)
+  putStrLn "Concluídas: "
+  withFile "atividades.txt" ReadMode (\handle -> do
+    contents <- hGetContents handle
+    let linhas = lines contents
+        atividadesConcluidas = filter (\linha -> (Atividade.status (read linha :: Atividade.Atividade)) == "Concluído") linhas
+    mapM_ putStrLn atividadesConcluidas)
+  putStrLn "Atrasadas: "
+  withFile "atividades.txt" ReadMode (\handle -> do
+    contents <- hGetContents handle
+    let linhas = lines contents
+        atividadesAtrasadas = filter (\linha -> (Atividade.status (read linha :: Atividade.Atividade)) == "Atrasado") linhas
+    mapM_ putStrLn atividadesAtrasadas)
