e2cf5c9fe941f543fc6421fc28a493f1262893e4
unknown <amanda.santana@ccc.ufcg.edu.br>
2023-06-08 15:54:41 -0300

    adiciona função cadastrar atividade

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -20,9 +20,9 @@ main :-
 
 escolherOpcao(1) :- usuario.
 escolherOpcao(2) :- atividade.
-escolherOpcao(3) :- exibirQuadro.
-escolherOpcao(4) :- editarAtividade.
-escolherOpcao(5) :- status.
+escolherOpcao(3) :- exibir_quadro.
+escolherOpcao(4) :- editar_atividade.
+escolherOpcao(5) :- editar_status.
 escolherOpcao(6) :- halt.
 escolherOpcao(_) :- writeln('Opção inválida!'), main.
 
@@ -39,9 +39,9 @@ usuario :-
     write('\n'),
     main.
 
-/*
+
 atividade :-
-    write('Insira o ID da atividade'),
+    write('Insira o ID da atividade: '),
     ler_string(IdAtv),
     writeln('Tarefa:' ),
     write('Insira o nome: '),
@@ -50,15 +50,62 @@ atividade :-
     ler_string(IdUsuario),
     writeln('Status:'),
     writeln('Status possiveis: A fazer | Em andamento | Concluído'),
-    write('Adicione um desses status a sua atividade'),
-    ler_string(status),
+    write('Adicione um desses status a sua atividade: '),
+    ler_string(Status),
+    (\+ verifica_status(Status) -> writeln('Entrada inválida, por favor escolha uma das opções solicitadas.')
+   ; write('Grau de urgência: '), 
+      write('Você precisa de ajuda com o grau de urgência? S/N '), 
+      read(Urgencia),
+      (Urgencia =:= S -> 
+      writeln('Matriz de Eisenhower:'),
+      writeln('+----------------+----------------+'),
+      writeln('| Urgente e      | Importante e   |'),
+      writeln('| importante     | não urgente    |'),
+      writeln('+----------------+----------------+'),
+      writeln('| Não urgente e  | Não importante |'),
+      writeln('| importante     | e urgente      |'),
+      writeln('+----------------+----------------+'),
+      writeLn('Escolha o grau de urgência:'),
+      writeLn('1 - Urgente e importante'),
+      writeLn('2 - Importante e não urgente'),
+      writeLn('3 - Não importante e urgente'),
+      writeLn('4 - Não urgente e não importante'),
+      writeLn('Opção: '),
+      ler_string(Opcao)),
 
+      % Verificar opcao
+
+      writeln('Dificuldade: '),
+        writeln('Fácil | Médio | Difícil'),
+        writeln('Adicione um desses níveis de dificuldade a sua atividade: '),
+        read(Dificuldade),
+
+        % Verificar dificuldade
+
+        writeln('Data de entrega: '),
+        read(Data),
+        cadastrar_atividade(IdAtv, Tarefa, IdUsuario, Status, Urgencia, Dificuldade, Data),
+        writeln('Atividade cadastrada com sucesso! \n');
+      (Urgencia =:= N -> 
+        writeln('Dificuldade: '),
+        writeln('Fácil | Médio | Difícil'),
+        writeln('Adicione um desses níveis de dificuldade a sua atividade: '),
+        read(Dificuldade),
+        writeln('Data de entrega: '),
+        read(Data),
+        cadastrar_atividade(IdAtv, Tarefa, IdUsuario, Status, Urgencia, Dificuldade, Data),
+        writeln('Atividade cadastrada com sucesso! \n')
+      );
+      (Urgencia =:= _ -> writeln('Opção inválida, por favor, escolha S ou N. \n'));
+      ),
     main.
+
   % implementar o resto das funcoes
   % matriz Eisenhower
   
 
-editarAtividade :-
+/*
+editar_atividade :-
     write('Qual é o identificador da atividade que você deseja editar?'),
     ler_string(IdAtividade),
     write('O que você deseja alterar?'),
@@ -76,7 +123,7 @@ editarAtividade :-
   % EditarAtividades.editarAtividade (read idAtividade) (Just novoNome) (Just novoDif) (Just novoUrg) (Just novoEntrega)
   % funcao de outro arquivo
 
-status :-
+editar_status :-
     write('Digite o id da atividade que deseja alterar: '),
     ler_string(IdAtividade),
     write('Digite o novo status da atividade: '),
@@ -85,4 +132,5 @@ status :-
   % EditarAtividades.editarStatus (read idAtividade) (Just novoStatus)
   % funcao de outro arquivo 
 
+exibir_quadro:-
 */
--- Prolog/utils.pl
+++ Prolog/utils.pl
@@ -6,3 +6,13 @@ ler_numero(N) :-
     read_line_to_codes(user_input, R),
     atom_string(R, X),
     atom_number(X, N).
+
+verifica_opcao(Opcao, Elem):-
+    Elem is ['1', '2', '3', '4'],
+    atom_string(Elem, R),
+    member(Opcao, R).
+
+verifica_status(A fazer):- true, !.
+verifica_status(Em andamento):- true, !.
+verifica_status(Concluido):- true, !.
+verifica_status(_):- false, !.
